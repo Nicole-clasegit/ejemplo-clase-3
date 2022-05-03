6f173f00cbbc61f57d2830932f9081b26c16da37
Nicole-clasegit <nicoleocampo108@icloud.com>
2022-05-03 11:17:10 -0300

    cambio generald e un archivo

--- ejercicioDoWhile.js
+++ ejercicioDoWhile.js
@@ -3,3 +3,8 @@ do {
     clave = prompt('Ingrese su password: ');
 } while (clave!=1234);
 alert('Acceso correcto!!! Bienvenido a la web!!!');
+var sueldo;
+if(sueldo>100)
+{
+    console.log("si es mayor que 100");
+}
