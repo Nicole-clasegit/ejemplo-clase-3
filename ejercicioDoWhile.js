a833c1cca355a87cc21396167011623ad8754523
Nicole-clasegit <nicoleocampo108@icloud.com>
2022-05-03 11:16:31 -0300

    cambios en general y nuevop txt

--- ejercicioDoWhile.js
+++ ejercicioDoWhile.js
@@ -1,5 +1,12 @@
 let clave;
 do {
     clave = prompt('Ingrese su password: ');
+    $('#test').hide();
 } while (clave!=1234);
 alert('Acceso correcto!!! Bienvenido a la web!!!');
+$("#cambio-de-otro-formContacto");
+
+
+
+//nuevo comentario de otro 
+var idEmpleado=10;
