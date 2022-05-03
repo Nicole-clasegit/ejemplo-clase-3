763b03f2ca1a084fa3d7f408c0f00144c18a0c9e
Nicole-clasegit <nicoleocampo108@icloud.com>
2022-05-03 10:27:33 -0300

    commit clase 3 con ramas

---
+++ LaboratorioMath2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LaboratorioMath2 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String palabra;
+		char letra;
+		int ascii;
+		System.out.print("Indique una palabra: ");
+		palabra = teclado.next();
+
+		ascii = (int) ((Math.random() * 25) + 97);
+
+		letra = (char) ascii;
+
+		System.out.println(
+				"La letra " + letra + " se encuentra en " + palabra + "? " + palabra.contains(String.valueOf(letra)));
+
+		teclado.close();
+
+	}
+
+}
